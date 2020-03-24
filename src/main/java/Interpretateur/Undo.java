06f6d484c7065fda865d23e1de02bace3bc46f62
uvsq21805839 <dskprod@gmail.com>
2020-03-24 08:39:48 +0100

    undo

--- src/main/java/Interpretateur/Undo.java
+++ src/main/java/Interpretateur/Undo.java
@@ -1,16 +1,41 @@
 
 package Interpretateur;
 
+import java.util.Stack;
+
 /*
  * @Diawara
  */
 
 public class Undo implements InterGeneric {
 
-	public void apply() {
-		// TODO Auto-generated method stub
-		
+	private Stack<Stack<Double>> undo;
+	
+	private Stack<Double> pile;
+	
+	public Undo(final Stack<Double> pileOperandes) {
+		pile = pileOperandes;
+		undo = new Stack<Stack<Double>>();
 	}
 	
-
+	public void alertChange() {
+		@SuppressWarnings("unchecked")
+		Stack<Double> s = (Stack<Double>) pile.clone();
+		undo.push(s);
+	}
+	
+	private void copyLastStack() {
+		for (double d : undo.lastElement()) {
+			pile.push(d);
+		}
+	}
+	
+	public void apply() {
+		while (!pile.isEmpty()) {
+			pile.pop();
+		}
+		
+		undo.pop();
+		copyLastStack();
+	}
 }
